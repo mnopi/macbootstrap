e3c2ab8f42aadb0db54370209545e68f43fdd926
José A. Puértolas <26859654+j5pu@users.noreply.github.com>
2020-03-29 16:24:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,13 @@
 ## START: script constants
 admin_user="yes"
 ## END: script constants
+
+USER_PASSWD="aaaa"
+security find-generic-password -s "${USER}" -a USER_PASSWD -w
+
+security add-generic-password -s "${USER}" -a "USER_PASSWD" -w "${USER_PASSWD}" -A -U
+security add-generic-password -s "${USER}" -a "USER_PASSWD" -w "${USER_PASSWD}" -A 
+
+USER_PASSWD
+
+echo "${USER_PASSWD}" | sudo -S true -k
