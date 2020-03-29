0b10d19a80827ac59c7985bc36117ff39a7f78a5
José A. Puértolas <26859654+j5pu@users.noreply.github.com>
2020-03-29 20:39:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,23 @@ ACCOUNT_NAME=$( [[ "$( uname -s )" == "Darwin" ]] && /usr/bin/stat -f "%Su" /dev
 SECRETS_D="/Users/${ACCOUNT_NAME}/Library/Mobile Documents/com~apple~CloudDocs/secrets/secrets.d"
 groups_sudoers="staff admin wheel"
 enableroot="/Users/${ACCOUNT_NAME}/.enableroot"
+tag="macbootstrap"
 admin_user="yes"
-
 ## END: script constants
 
 test -f "${SECRETS_D}"/*.sh && source "${SECRETS_D}"/*.sh || { echo "- Error: Not secrets files ${SECRETS_D}" >&2; exit 1; }
 [[ "${ACCOUNT_PASSWD-}" ]] || { echo "- Error: ACCOUNT_PASSWD empty" >&2; exit 1; }
 echo "${ACCOUNT_PASSWD}" | sudo -S true >/dev/null 2>&1 || { echo "- Error: ACCOUNT_PASSWD password - incorrect" >&2; exit 1; }
 
-## START: /etc/sudoers.d
+## BEGIN: script functions
+tag_file() {
+if ! ${2} xattr -w com.apple.metadata:_kMDItemUserTags "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><array><string>${tag}</string></array></plist>" "${1}"; then
+  echo "= Warning: tag "${1}" - not set" >&2
+fi
+}
+## END: script functions
+
+# START: /etc/sudoers.d
 for group_sudoers in ${groups_sudoers}; do
   if [[ ! -f "/etc/sudoers.d/${group_sudoers}" ]]; then
     echo "%${group_sudoers} ALL=(ALL) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/${group_sudoers}" >/dev/null 2>&1
@@ -22,13 +30,39 @@ for group_sudoers in ${groups_sudoers}; do
     echo 'Defaults env_delete = "HOME"' | sudo tee -a "/etc/sudoers.d/${group_sudoers}" >/dev/null 2>&1
     echo 'Defaults env_delete = "PS1"' | sudo tee -a "/etc/sudoers.d/${group_sudoers}" >/dev/null 2>&1
     echo "Defaults: %${group_sudoers} !logfile, !syslog" | sudo tee -a "/etc/sudoers.d/${group_sudoers}" >/dev/null 2>&1
+    tag_file "/etc/sudoers.d/${group_sudoers}" sudo
     echo "+ OK: /etc/sudoers.d/${group_sudoers}"
   fi
 done
 ## END: /etc/sudoers.d
 
 ## START: enable root
-test -f "${enableroot}" || { dsenableroot -u "${ACCOUNT_NAME}" -p "${ACCOUNT_PASSWD}" -r "${ACCOUNT_PASSWD}" && echo "+ OK: root enabled" || echo "- Error: enable root" >&2 }
+test -f "${enableroot}" || { dsenableroot -u "${ACCOUNT_NAME}" -p "${ACCOUNT_PASSWD}" -r "${ACCOUNT_PASSWD}" && { touch "${enableroot}"; tag_file "${enableroot}"; echo "+ OK: root enabled"; } || echo "- Error: enable root" >&2; }
 ## END: enable root
 
+## BEGIN: csrutil disable
+if ! csrutil status | grep disabled > /dev/null 2>&1; then
+  echo " - Disable csrutil to continue: csrutil disable"
+  csrutil_change="yes"
+  read -n 1 -s -r -p "Press any key to reboot: "
+  echo
+  sudo reboot
+fi
+## END: csrutil disable
 
+## BEGIN: Master disable (Applications open internet)
+if sudo spctl --status | /usr/bin/grep enabled >/dev/null 2>&1; then
+  sudo spctl --master-disable
+  echo "+ OK: spctl master - disable"
+fi
+## END: Master disable (Applications open internet)
+
+## BEGIN: ipv6
+while read -r interface; do
+  networksetup -getinfo  "${interface}" | grep "IPv6: Off" > /dev/null 2>&1 || { sudo networksetup -setv6off  "${interface}"; echo "+ OK: IPv6 ${interface} - Off"; }
+done < <( networksetup -listallnetworkservices | grep -v "asterisk" )
+## ENF: ipv6
+
+## BEGIN: locate database
+sudo launchctl print system | grep \"com.apple.locate\" >/dev/null 2>&1 && echo "+ OK: locate database - loaded" || sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist && sudo launchctl enable system/com.apple.locate
+## ENF: locate database
