5a17ea1ac8c4a4068f8151e0e3755269e740a4df
José A. Puértolas <26859654+j5pu@users.noreply.github.com>
2020-03-29 18:08:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ SECRETS_D="/Users/${ACCOUNT_NAME}/Library/Mobile Documents/com~apple~CloudDocs/s
 admin_user="yes"
 ## END: script constants
 
-test -f "${SECRETS_D}"/*.sh && source "${SECRETS_D}"/*.sh || { echo "- Error: Not secrets files ${SECRETS_D}" >&2; exit 1; } )
+test -f "${SECRETS_D}"/*.sh && source "${SECRETS_D}"/*.sh || { echo "- Error: Not secrets files ${SECRETS_D}" >&2; exit 1; }
+[[ ! "${ACCOUNT_PASSWD-}" ]] || { echo "- Error: ACCOUNT_PASSWD empty" >&2; exit 1; }
+echo "${ACCOUNT_PASSWD}"
