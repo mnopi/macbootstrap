6d2f3ba904a464f506cbe9748425b7ad0d7c50ef
José A. Puértolas <26859654+j5pu@users.noreply.github.com>
2020-03-30 11:56:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@ ACCOUNT_NAME=$( [[ "$( uname -s )" == "Darwin" ]] && /usr/bin/stat -f "%Su" /dev
 
 ## START: script constants
 SECRETS_D="/Users/${ACCOUNT_NAME}/Library/Mobile Documents/com~apple~CloudDocs/secrets/secrets.d"
+SSH="/Users/${ACCOUNT_NAME}/Library/Mobile Documents/com~apple~CloudDocs/secrets/ssh"
 groups_sudoers="staff admin wheel"
 enableroot="/Users/${ACCOUNT_NAME}/.enableroot"
 enablelocate="/Users/${ACCOUNT_NAME}/.enablelocate"
-tag="macbootstrap"
+repo="macbootstrap"
+tag="${repo}"
+github_user="${ACCOUNT_NAME}"
 admin_user="yes"
+admin_id="502"
+apple_scripts="/Users/${ACCOUNT_NAME}/Library/Mobile Documents/com~apple~ScriptEditor2/Documents"
 ## END: script constants
 
 test -f "${SECRETS_D}"/*.sh && source "${SECRETS_D}"/*.sh || { echo "- Error: Not secrets files ${SECRETS_D}" >&2; exit 1; }
@@ -77,3 +82,126 @@ log_dir="/var/log/${ACCOUNT_NAME}"
 test -f "/Users/${ACCOUNT_NAME}/.hushlogin" || { touch "/Users/${ACCOUNT_NAME}/.hushlogin"; tag_file "/Users/${ACCOUNT_NAME}/.hushlogin"; echo "+ OK: /Users/${ACCOUNT_NAME}/.hushlogin - created"; }
 sudo test -f /var/root/.hushlogin || { sudo touch /var/root/.hushlogin; tag_file /var/root/.hushlogin sudo; echo "+ OK: /var/root/.hushlogin - created"; }
 ## END: .hushlogin
+
+## BEGIN: .ssh
+if [[ ! -d "/Users/${ACCOUNT_NAME}/.ssh" ]]; then
+  mkdir "/Users/${ACCOUNT_NAME}/.ssh"; chmod go-rx "/Users/${ACCOUNT_NAME}/.ssh"
+  tag_file "/Users/${ACCOUNT_NAME}/.ssh"; echo "+ OK: /Users/${ACCOUNT_NAME}/.ssh - created"
+fi
+
+if ! sudo test -d /var/root/.ssh; then
+  sudo mkdir "/var/root/.ssh"; sudo chmod go-rx "/var/root/.ssh"
+  tag_file "/var/root/.ssh" sudo; echo "+ OK: /var/root/.ssh - created"
+fi
+
+test -f "${SSH}/${ACCOUNT_NAME}/id_rsa" || { echo "- Error: ${SSH}/${ACCOUNT_NAME}/id_rsa - not found" >&2; exit 1; }
+
+if ! test -f "${SSH}/${ACCOUNT_NAME}/authorized_keys"; then
+  curl -s "https://github.com/${github_user}.keys" > "${SSH}/${ACCOUNT_NAME}/authorized_keys"
+  if grep ssh-rsa "${SSH}/${ACCOUNT_NAME}/authorized_keys" > /dev/null 2>&1; then
+    tag_file "${SSH}/${ACCOUNT_NAME}/authorized_keys"
+    echo "+ OK: ${SSH}/${ACCOUNT_NAME}/authorized_keys - downloaded"
+  else
+    rm -f "${SSH}/${ACCOUNT_NAME}/authorized_keys"
+    echo "- Error: ${SSH}/${ACCOUNT_NAME}/authorized_keys - not downloaded" >&2; exit 1
+  fi
+fi
+
+if [[ ! -f "/Users/${ACCOUNT_NAME}/.ssh/id_rsa" ]]; then
+  cp "${SSH}/${ACCOUNT_NAME}/id_rsa" "/Users/${ACCOUNT_NAME}/.ssh/id_rsa" && chmod go-rwx "/Users/${ACCOUNT_NAME}/.ssh/id_rsa"
+  tag_file "/Users/${ACCOUNT_NAME}/.ssh/id_rsa"; echo "+ OK: /Users/${ACCOUNT_NAME}/.ssh/id_rsa - installed"
+fi
+
+if ! sudo test -f /var/root/.ssh/id_rsa; then
+  sudo cp "${SSH}/${ACCOUNT_NAME}/id_rsa" "/var/root/.ssh/id_rsa" && sudo chmod go-rwx /var/root/.ssh/id_rsa
+  tag_file "/var/root/.ssh/id_rsa" sudo; echo "+ OK: /var/root/.ssh/id_rsa - installed"
+fi
+
+if [[ ! -f "/Users/${ACCOUNT_NAME}/.ssh/authorized_keys" ]]; then
+  cp "${SSH}/${ACCOUNT_NAME}/authorized_keys" "/Users/${ACCOUNT_NAME}/.ssh/authorized_keys" && chmod go-rwx "/Users/${ACCOUNT_NAME}/.ssh/authorized_keys"
+  tag_file "/Users/${ACCOUNT_NAME}/.ssh/authorized_keys"; echo "+ OK: /Users/${ACCOUNT_NAME}/.ssh/authorized_keys - installed"
+fi
+
+if ! sudo test -f /var/root/.ssh/authorized_keys; then
+  sudo cp "${SSH}/${ACCOUNT_NAME}/authorized_keys" "/var/root/.ssh/authorized_keys" && sudo chmod go-rwx /var/root/.ssh/authorized_keys
+  tag_file "/var/root/.ssh/authorized_keys" sudo; echo "+ OK: /var/root/.ssh/authorized_keys - installed"
+fi
+## END: .ssh
+
+## BEGIN: admin user
+if [[ "${admin_user}" == "yes" ]] && ! sudo dscl . -read /Users/admin > /dev/null 2>&1; then
+  if ! id "${admin_id}" > /dev/null 2>&1; then
+    sudo dscl . -create /Users/admin
+    sudo dscl . -create /Users/admin UserShell /bin/bash
+    sudo dscl . -create /Users/admin RealName “Admin”
+    sudo dscl . -create /Users/admin UniqueID "${admin_id}"
+    sudo dscl . -create /Users/admin PrimaryGroupID $( id -g ${ACCOUNT_NAME} )
+    sudo dscl . -create /Users/admin NFSHomeDirectory /Users/admin
+    sudo dscl . -passwd /Users/admin "${ACCOUNT_PASSWD}"
+    sudo dscl . -append /Groups/admin GroupMembership admin
+    echo "+ OK: User admin - created"
+  else
+    echo "- Error: admin_id ${admin_id} - used" >&2; exit 1;
+  fi
+fi
+## END: admin user
+
+## BEGIN: iCloud
+if test -d "${HOME}/Library/Mobile Documents/com~apple~CloudDocs/"; then
+  for dir in "${HOME}/iCloud" "${HOME}/Desktop/iCloud"; do
+    test -d "${dir}" && test -L "${dir}" || { ln -s "${HOME}/Library/Mobile Documents/com~apple~CloudDocs/" "${dir}"; tag_file "${dir}"; echo "+ OK: ${dir} link - created"; }
+  done
+else
+  echo "- Error: ${HOME}/Library/Mobile Documents/com~apple~CloudDocs - not found" >&2; exit 1;
+fi
+## END: iCloud
+
+## BEGIN: Remote Login
+if [[ "$( sudo systemsetup -getremotelogin | awk '{print $3}' )" == "Off" ]]; then
+  killall "System Preferences"
+  test -f "${apple_scripts}/remote-login-click.scpt" || { echo "- Error: '${apple_scripts}/remote-login-click.scpt' not found" >&2; exit 1; }
+  osascript "${apple_scripts}/remote-login-click.scpt" > /dev/null 2>&1
+  sudo sqlite3 '/Library/Application Support/com.apple.TCC/TCC.db' "update access set allowed=1"
+  if [[ "$( sudo systemsetup -getremotelogin | awk '{print $3}' )" == "Off" ]]; then
+    osascript "${apple_scripts}/remote-login-click.scpt" || exit 1
+  fi
+  if [[ "$( sudo systemsetup -getremotelogin | awk '{print $3}' )" == "Off" ]]; then
+    echo "- Error: Remote Login - Off" >&2; exit 1;
+  else
+    echo "+ OK: Remote Login - On"
+  fi
+fi
+## END: Remote Login
+
+## BEGIN: Printer
+if ! lpstat -p HP 2>/dev/null | grep enable >/dev/null 2>&1; then
+  # /etc/cups/ppd/HP.ppd
+  test -d "${HOME}/Library/Printers" || mkdir -p "${HOME}/Library/Printers"
+
+  url="https://raw.githubusercontent.com/${ACCOUNT_NAME}/${repo}/master/files/HP.ppd"
+  curl -sL "${url}?$(date +%s)" > "${HOME}/Library/Printers/HP.ppd"
+
+  if grep PPD-Adobe "${HOME}/Library/Printers/HP.ppd" > /dev/null 2>&1; then
+    echo "+ OK: ${url} - downloaded"
+  else
+    rm -f "${HOME}/Library/Printers/HP.ppd"
+    echo "- Error: ${url} - not downloaded" >&2; exit 1
+  fi
+
+  lpadmin -p HP -v "dnssd://HP._ipps._tcp.local." -P "${HOME}/Library/Printers/HP.ppd" -o printer-is-shared=false >/dev/null 2>&1
+  cupsenable HP -E >/dev/null 2>&1
+  cupsaccept HP >/dev/null 2>&1
+
+  if lpstat -p HP | grep enable >/dev/null 2>&1; then
+    echo "+ OK: Printer - installed"
+  else
+    echo "- Error: Printer - install" >&2; exit 1;
+  fi
+fi
+## END: Printer
+
+## BEGIN: install brew
+if [[ ! -f /usr/local/bin/brew ]]; then
+  yes yes | /usr/bin/ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+fi
+## END: install brew
