f36311c84c4e97f17d1e84ff00c28f746ac3d0f6
José A. Puértolas <26859654+j5pu@users.noreply.github.com>
2020-03-30 14:06:40 +0200

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
+
+ACCOUNT_NAME=$( [[ "$( uname -s )" == "Darwin" ]] && /usr/bin/stat -f "%Su" /dev/console || { echo "- Error: Not macOS" >&2; exit 1; } )
+
+## START: script constants
+repo="macbootstrap"
+tag="${repo}"
+github_user="${ACCOUNT_NAME}"
+file="npmfile"
+url="https://raw.githubusercontent.com/${ACCOUNT_NAME}/${repo}/master/packages/${file}"
+## END: script constants
+
+curl -sL "${url}?$(date +%s)" > "/tmp/${file}"
+
+if grep "${file}" "/tmp/${file}" > /dev/null 2>&1; then
+  echo "+ OK: ${url} - downloaded"
+else
+  rm -f "/tmp/${file}"
+  echo "- Error: ${url} - not downloaded" >&2; exit 1
+fi
+
+npm install -g $( cat "/tmp/${file}" | grep -v "#" )
+rm -f "/tmp/${file}"
 
