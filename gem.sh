ecb562f813b929673a4d04f851dadb55007fe307
José A. Puértolas <26859654+j5pu@users.noreply.github.com>
2020-03-30 14:06:22 +0200

    Update gem.sh

--- gem.sh
+++ gem.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
+
+ACCOUNT_NAME=$( [[ "$( uname -s )" == "Darwin" ]] && /usr/bin/stat -f "%Su" /dev/console || { echo "- Error: Not macOS" >&2; exit 1; } )
+
+## START: script constants
+repo="macbootstrap"
+tag="${repo}"
+github_user="${ACCOUNT_NAME}"
+file="Gemfile"
+url="https://raw.githubusercontent.com/${ACCOUNT_NAME}/${repo}/master/packages/${file}"
+## END: script constants
+
+curl -sL "${url}?$(date +%s)" > "/tmp/${file}"
+
+if grep "${file}" "/tmp/${file}" > /dev/null 2>&1; then
+  echo "+ OK: ${url} - downloaded"
+else
+  rm -f "/tmp/${file}"
+  echo "- Error: ${url} - not downloaded" >&2; exit 1
+fi
+
+/usr/local/opt/ruby/bin/bundle --gemfile="/tmp/${file}" --system  # --system ~= --path="${GEMS_PATH}"
+rm -f "/tmp/${file}"
 
