1e865be9b4cec658d40eb3808ec41c959f84fc9d
José A. Puértolas <26859654+j5pu@users.noreply.github.com>
2020-03-30 11:53:45 +0200

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+ACCOUNT_NAME=$( [[ "$( uname -s )" == "Darwin" ]] && /usr/bin/stat -f "%Su" /dev/console || { echo "- Error: Not macOS" >&2; exit 1; } )
+
+## START: script constants
+repo="macbootstrap"
+tag="${repo}"
+github_user="${ACCOUNT_NAME}"
+## END: script constants
+
+url="https://raw.githubusercontent.com/${ACCOUNT_NAME}/${repo}/master/packages/Brewfile"
+curl -sL "${url}?$(date +%s)" > /tmp/Brewfile
+
+if grep brew /tmp/Brewfile > /dev/null 2>&1; then
+  echo "+ OK: ${url} - downloaded"
+else
+  rm -f /tmp/Brewfile
+  echo "- Error: ${url} - not downloaded" >&2; exit 1
+fi
+
+brew bundle --file /tmp/Brewfile
+brew bundle cleanup --force --file /tmp/Brewfile
