e40c3633261d15badb977cd0e1c32d7baff39cee
José A. Puértolas <26859654+j5pu@users.noreply.github.com>
2020-03-30 14:07:01 +0200

    Update pip.sh

--- pip.sh
+++ pip.sh
@@ -6,16 +6,18 @@ ACCOUNT_NAME=$( [[ "$( uname -s )" == "Darwin" ]] && /usr/bin/stat -f "%Su" /dev
 repo="macbootstrap"
 tag="${repo}"
 github_user="${ACCOUNT_NAME}"
+file="requirements.txt"
+url="https://raw.githubusercontent.com/${ACCOUNT_NAME}/${repo}/master/packages/${file}"
 ## END: script constants
 
-url="https://raw.githubusercontent.com/${ACCOUNT_NAME}/${repo}/master/packages/requirements.txt"
-curl -sL "${url}?$(date +%s)" > /tmp/requirements.txt
+curl -sL "${url}?$(date +%s)" > "/tmp/${file}"
 
-if grep ansible /tmp/requirements.txt > /dev/null 2>&1; then
+if grep "${file}" "/tmp/${file}" > /dev/null 2>&1; then
   echo "+ OK: ${url} - downloaded"
 else
-  rm -f /tmp/requirements.txt
+  rm -f "/tmp/${file}"
   echo "- Error: ${url} - not downloaded" >&2; exit 1
 fi
 
-pip3 install --upgrade -r /tmp/requirements.txt
+pip3 install --upgrade -r "/tmp/${file}"
+rm -f "/tmp/${file}"
